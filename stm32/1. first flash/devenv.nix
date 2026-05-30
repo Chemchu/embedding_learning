6b00f58f14460145a0f35d583082c119985c6154
Chemchu <gustavo_9_rj@hotmail.com>
2026-05-30 23:22:01 +0100

    feat: added devenv shell

---
+++ stm32/1. first flash/devenv.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  packages = [
+    pkgs.gcc-arm-embedded
+    pkgs.stlink
+    pkgs.gnumake
+  ];
+
+  # Generate .clangd with the actual nix store paths from the active toolchain.
+  enterShell = ''
+    sysroot=$(arm-none-eabi-gcc -print-sysroot)
+    gcc_include=$(arm-none-eabi-gcc -print-file-name=include)
+    cat > .clangd <<CLANGD
+CompileFlags:
+  Compiler: arm-none-eabi-gcc
+  Add:
+    - -I.
+    - -Icmsis_core/CMSIS/Core/Include
+    - -Icmsis_u0/Include
+    - --target=arm-none-eabi
+    - -mcpu=cortex-m0plus
+    - -mthumb
+    - -mfloat-abi=soft
+    - -DSTM32U083xx
+    - -nostdinc
+    - -isystem$sysroot/include
+    - -isystem$gcc_include
+CLANGD
+  '';
+}

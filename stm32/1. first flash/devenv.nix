d80f9acb2d30b9e8d70d63c5451069e72efde565
Chemchu <gustavo_9_rj@hotmail.com>
2026-05-31 01:01:59 +0100

    feat: fixed makefile and added devenv

--- stm32/1. first flash/devenv.nix
+++ stm32/1. first flash/devenv.nix
@@ -1,31 +1,29 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   packages = [
     pkgs.gcc-arm-embedded
-    pkgs.stlink
     pkgs.gnumake
+    pkgs.probe-rs-tools
   ];
 
   # Generate .clangd with the actual nix store paths from the active toolchain.
   enterShell = ''
-    sysroot=$(arm-none-eabi-gcc -print-sysroot)
-    gcc_include=$(arm-none-eabi-gcc -print-file-name=include)
-    cat > .clangd <<CLANGD
-CompileFlags:
-  Compiler: arm-none-eabi-gcc
-  Add:
-    - -I.
-    - -Icmsis_core/CMSIS/Core/Include
-    - -Icmsis_u0/Include
-    - --target=arm-none-eabi
-    - -mcpu=cortex-m0plus
-    - -mthumb
-    - -mfloat-abi=soft
-    - -DSTM32U083xx
-    - -nostdinc
-    - -isystem$sysroot/include
-    - -isystem$gcc_include
-CLANGD
+        sysroot=$(arm-none-eabi-gcc -print-sysroot)
+        gcc_include=$(arm-none-eabi-gcc -print-file-name=include)
+        cat > .clangd <<CLANGD
+    CompileFlags:
+      Compiler: arm-none-eabi-gcc
+      Add:
+        - -I.
+        - -Icmsis_core/CMSIS/Core/Include
+        - -Icmsis_u0/Include
+        - --target=arm-none-eabi
+        - -mcpu=cortex-m0plus
+        - -mthumb
+        - -mfloat-abi=soft
+        - -DSTM32U083xx
+        - -nostdinc
+        - -isystem$sysroot/include
+        - -isystem$gcc_include
+    CLANGD
   '';
 }
